9b5cd35e13205b83260c07cfd17be3b1fc2192ab
wlb123git <109083282+wlb123git@users.noreply.github.com>
2025-05-05 10:10:16 +0800

    修改BoardConfig.h，D+位于10

--- configs/GamepadConverter/BoardConfig.h
+++ configs/GamepadConverter/BoardConfig.h
@@ -21,8 +21,8 @@
 #define GPIO_PIN_07 GpioAction::BUTTON_PRESS_B2     // B2     | B      | A       | Circle   | 3      | K2     |
 #define GPIO_PIN_08 GpioAction::BUTTON_PRESS_R2     // R2     | RT     | ZR      | R2       | 8      | K3     |
 #define GPIO_PIN_09 GpioAction::BUTTON_PRESS_L2     // L2     | LT     | ZL      | L2       | 7      | K4     |
-#define GPIO_PIN_10 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
-#define GPIO_PIN_11 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
+// #define GPIO_PIN_10 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
+// #define GPIO_PIN_11 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
 #define GPIO_PIN_12 GpioAction::BUTTON_PRESS_R1     // R1     | RB     | R       | R1       | 6      | P3     |
 #define GPIO_PIN_13 GpioAction::BUTTON_PRESS_L1     // L1     | LB     | L       | L1       | 5      | P4     |
 #define GPIO_PIN_00 GpioAction::BUTTON_PRESS_S1     // S1     | Back   | Minus   | Select   | 9      | Coin   |
@@ -60,7 +60,7 @@
 #define KEY_BUTTON_FN   -1                    // Hotkey Function                                        |
 
 #define USB_PERIPHERAL_ENABLED 1
-#define USB_PERIPHERAL_PIN_DPLUS 28
+#define USB_PERIPHERAL_PIN_DPLUS 10
 #define USB_PERIPHERAL_PIN_ORDER 0
 #define USB_PERIPHERAL_PIN_5V -1
 
